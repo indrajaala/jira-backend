20d8981e14f6bdb6a72092c7a928b9beb07a5663
Minugonda Madhu <Minugonda.Madhu@civica2.com>
2023-04-17 20:37:35 +0530

    m

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,8 +3,10 @@ import modelPost from "../models/modelPost.js";
 export const getPosts = async (req, res) => {
   try {
     const modelPosts = await modelPost.find();
+    console.log("HELLO World")
     res.status(200).json(modelPosts);
   } catch (error) {
+    console.log("error world");
     res.status(404).json({ message: error.message });
   }
 };
