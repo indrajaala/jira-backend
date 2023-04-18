c9aa145637e5e0de3509739895c978e6e93fdf65
Minugonda Madhu <Minugonda.Madhu@civica2.com>
2023-04-18 11:53:40 +0530

    m

--- routes/issues.js
+++ routes/issues.js
@@ -2,5 +2,5 @@ import express from "express";
 import { getIssues } from "../controllers/issues.js";
 
 const router = express.Router();
-router.get("/", getIssues());
+router.get("/", getIssues);
 export default router;
