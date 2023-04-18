6f09a4619d6afc57075bd98a09e76305c8790363
Minugonda Madhu <Minugonda.Madhu@civica2.com>
2023-04-18 12:47:13 +0530

    m

--- controllers/issues.js
+++ controllers/issues.js
@@ -23,9 +23,8 @@ export const createIssues = async (req, res) => {
 
 export const updateIssues = async (req, res) => {
     try{
-        console.log("the params are",req.query);
-        // const issue = await modelIssue.updateOne({_id:req.params.id},{＄set: req.body});
-        // res.status(200).json(issue);
+        const issue = await modelIssue.updateOne({_id:req.query.id},{＄set: req.body});
+        res.status(200).json(issue);
 
     } catch (error) {
         console.log("error world");
