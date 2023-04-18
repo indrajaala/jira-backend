0539b2dd566882195e403bd815ae61a484a9fd53
Minugonda Madhu <Minugonda.Madhu@civica2.com>
2023-04-18 19:38:18 +0530

    m

--- controllers/projects.js
+++ controllers/projects.js
@@ -12,6 +12,8 @@ export const getProjects = async (req, res) => {
 
 export const updateProjects = async (req, res) => {
     try {
+        console.log("the id is", req.query.id);
+        console.log("the body is", req.body);
         const projects = await modelProject.updateOne({_id:req.query.id}, { $set: req.body });
         res.status(200).json(projects);
     } catch (error) {
