2a41d27126b4a87e3fa2fdae5a8d4d1501ceb078
Minugonda Madhu <Minugonda.Madhu@civica2.com>
2023-04-20 03:04:05 +0530

    modifiedd

--- controllers/issues.js
+++ controllers/issues.js
@@ -2,7 +2,7 @@ import modelIssue from "../models/modelIssue.js";
 
 export const getIssues = async (req, res) => {
     try {
-        const issues = await modelIssue.find().sort({index: 'desc'});
+        const issues = await modelIssue.find().sort({index: 'asc'});
         res.status(200).json(issues);
     } catch (error) {
         console.log("error world");
